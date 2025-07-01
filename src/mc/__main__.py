71bc6b63a01a17c8c1027533d4a496644e7f61f7
Michael Lamertz <michael.lamertz@gmail.com>
2025-07-01 07:02:41 +0200

    Snapshot

--- src/mc/__main__.py
+++ src/mc/__main__.py
@@ -1,5 +1,6 @@
 #!/bin/env python3
 
+from os import environ
 from types import SimpleNamespace
 
 import pygame
@@ -15,27 +16,34 @@ from mc.states.game import Game
 
 # cache.debug = True
 
-def load_spritesheet(renderer, fname):
+def load_atlas(renderer, fname):
     spritesheet = pygame.image.load(fname)
     t = sdl2.Texture.from_surface(renderer, spritesheet)
     cache.add(t, 'spritesheet')
 
     cache.create_atlas('spritesheet')
+    cache.add_to_atlas('spritesheet', 'crosshair', G.SPRITESHEET_CROSSHAIR)
+    cache.add_to_atlas('spritesheet', 'target', G.SPRITESHEET_TARGET)
     cache.add_to_atlas('spritesheet', 'missile', G.SPRITESHEET_MISSILE)
     cache.add_to_atlas('spritesheet', 'big-city', G.SPRITESHEET_BIG_CITY)
     cache.add_to_atlas('spritesheet', 'city', G.SPRITESHEET_CITY)
-    cache.add_to_atlas('spritesheet', 'crosshair', G.SPRITESHEET_CROSSHAIR)
+    cache.add_to_atlas('spritesheet', 'alien-big-green', G.SPRITESHEET_ALIEN_BIG_GREEN)
+    cache.add_to_atlas('spritesheet', 'alien-big-red', G.SPRITESHEET_ALIEN_BIG_RED)
+    cache.add_to_atlas('spritesheet', 'alien-green', G.SPRITESHEET_ALIEN_GREEN)
+    cache.add_to_atlas('spritesheet', 'alien-red', G.SPRITESHEET_ALIEN_RED)
+    cache.add_to_atlas('spritesheet', 'plane-green', G.SPRITESHEET_PLANE_GREEN)
+    cache.add_to_atlas('spritesheet', 'plane-red', G.SPRITESHEET_PLANE_RED)
+    cache.add_to_atlas('spritesheet', 'smartbomb-green', G.SPRITESHEET_SMARTBOMB_GREEN)
+    cache.add_to_atlas('spritesheet', 'smartbomb-red', G.SPRITESHEET_SMARTBOMB_RED)
+
     cache.add_to_atlas('spritesheet', 'ground', G.SPRITESHEET_GROUND)
 
 
 def main():
-    app = App(G.TITLE, G.SCREEN, G.FPS, bgcolor=G.COLOR.background)
-    app.renderer.logical_size = G.SCREEN.size
-    pygame.mouse.set_relative_mode(True)
-    app.renderer.set_viewport(app.renderer.get_viewport().move_to(topleft=(0,0)))
-    print(app.renderer.get_viewport())
+    app = App(G.TITLE, resolution=G.SCREEN.size, fps=G.FPS)
+    pygame.mouse.set_visible(False)
 
-    load_spritesheet(app.renderer, G.ASSETS.joinpath('spritesheet.png'))
+    load_atlas(app.renderer, G.ASSETS.joinpath('spritesheet.png'))
 
     states = SimpleNamespace(game=Game(app))
     app.state_machine.add(states.game, states.game)

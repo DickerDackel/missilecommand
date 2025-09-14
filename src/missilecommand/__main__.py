60d2f2c810bd6d887380fd25cb72494848abfc9f
Michael Lamertz <michael.lamertz@gmail.com>
2025-09-14 16:43:31 +0200

    Fullscreen by default

--- src/missilecommand/__main__.py
+++ src/missilecommand/__main__.py
@@ -84,9 +84,9 @@ def main() -> None:
     ][2 - opts.verbose + opts.quiet]
     logging.basicConfig(level=log_level)  # noqa: E402
 
-    w = pygame.Window(size=(1280, 960), position=pygame.WINDOWPOS_CENTERED)
-    app = App(C.TITLE, window=w, resolution=C.SCREEN.size, fps=C.FPS, bgcolor=C.COLOR.background)
-    # app = App(C.TITLE, resolution=C.SCREEN.size, fps=C.FPS, bgcolor=C.COLOR.background)
+    # w = pygame.Window(size=(1280, 960), position=pygame.WINDOWPOS_CENTERED)
+    # app = App(C.TITLE, window=w, resolution=C.SCREEN.size, fps=C.FPS, bgcolor=C.COLOR.background)
+    app = App(C.TITLE, resolution=C.SCREEN.size, fps=C.FPS, bgcolor=C.COLOR.background)
     pygame.mouse.set_visible(False)
 
     load_sounds(C.ASSETS)

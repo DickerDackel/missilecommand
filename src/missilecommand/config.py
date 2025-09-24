9d6aa2cf19b3500a9d26216865150c98f2261a92
Michael Lamertz <michael.lamertz@gmail.com>
2025-09-24 19:31:09 +0200

    Pause key mentioned

--- src/missilecommand/config.py
+++ src/missilecommand/config.py
@@ -393,10 +393,11 @@ MESSAGES = {
     },
     'instructions': {
         'INSTRUCTIONS': MessageConfig('INSTRUCTIONS', GRID(15, 8, 2, 2).center, 'center', COLOR.instructions, (2, 2)),
-        'SPACE': MessageConfig('PRESS SPACE TO PLAY', GRID(15, 14, 2, 1).center, 'center', COLOR.instructions),
-        'ESCAPE': MessageConfig('PRESS ESCAPE TO EXIT', GRID(15, 16, 2, 1).center, 'center', COLOR.instructions),
-        'MOUSE': MessageConfig('USE MOUSE CURSOR TO AIM', GRID(15, 18, 2, 1).center, 'center', COLOR.instructions),
-        'FIRE': MessageConfig('PRESS Q/W/A TO FIRE', GRID(15, 20, 2, 1).center, 'center', COLOR.instructions),
+        'SPACE': MessageConfig('PRESS SPACE TO PLAY', GRID(15, 13, 2, 1).center, 'center', COLOR.instructions),
+        'ESCAPE': MessageConfig('PRESS ESCAPE TO EXIT', GRID(15, 15, 2, 1).center, 'center', COLOR.instructions),
+        'MOUSE': MessageConfig('USE MOUSE CURSOR TO AIM', GRID(15, 17, 2, 1).center, 'center', COLOR.instructions),
+        'FIRE': MessageConfig('PRESS Q/W/A TO FIRE', GRID(15, 19, 2, 1).center, 'center', COLOR.instructions),
+        'PAUSE': MessageConfig('PRESS P TO PAUSE', GRID(15, 21, 2, 1).center, 'center', COLOR.instructions),
     },
     'pause': {
         'PAUSE': MessageConfig('PAUSE', GRID(15, 4, 2, 2).center, 'center', COLOR.normal_text, (3, 3)),
